2baf97c2e29ec04446ba821e8b8b6c89836264ee
Nate B <nathanbrengman@gmail.com>
2022-07-05 17:23:14 -0500

    added variables for the time

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+
+var row1 = document.querySelector('#hour-0')
+var row2 = document.querySelector('#hour-1')
+var row3 = document.querySelector('#hour-2')
+var row4 = document.querySelector('#hour-3')
+var row5 = document.querySelector('#hour-4')
+var row6 = document.querySelector('#hour-5')
+var row7 = document.querySelector('#hour-6')
+var row8 = document.querySelector('#hour-7')
+var row9 = document.querySelector('#hour-8')
